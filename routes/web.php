3ab241382b17a22f3003e31bfe1dbe76d8a43194
Ayelén Iasich <87945150+AyelenIasich@users.noreply.github.com>
2022-11-02 15:06:04 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('artista.home');
+    return view('welcome');
 });
 
 Route::resource('artista', ArtistaController::class);
