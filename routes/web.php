5d6a9b2e2c4f3eb64207c5392cf1f606594148bd
RuizVeraPablo <116479972+RuizVeraPablo@users.noreply.github.com>
2022-11-25 16:42:46 -0300

    Modificacion LOGIN

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login'); //PABLO - REEMPLAZO DE LA VISTA "WELCOME" POR LA VISTA DEL LOGIN ("auth.login")
 });
 
-Route::resource('artista', ArtistaController::class);
+Route::resource('artista', ArtistaController::class)->middleware('auth'); //PABLO - ..."->middleware('auth')" AGREGADO PARA QUE NO SE PUEDA ACCEDER A MENOS QUE SE ESTE LOGEADO
+
+Auth::routes(['register'=>false,'reset'=>false]); //PABLO - "['register'=>false,'reset'=>false]" AGREGADO PARA QUE DESAPAREZCAN LAS OPCIONES DE "REGISTER" Y "FORGOT YOUR PASSWORD?"
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
