5bdc854f7dc0a8d9d2a1359531fdf9a5a231fb2a
Ayelén Iasich <87945150+AyelenIasich@users.noreply.github.com>
2022-12-19 23:39:48 -0300

    Home controller mural y event paginate (4)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,8 +16,8 @@ class HomeController extends Controller
     {
        $home['home'] = Home::first();
         $artistas['artistas'] = Artista::all();
-        $murales = Mural::paginate(6);
-        $events = Event::where('fecha_evento', '>=', now())->with(['artists'])->paginate(6);
+        $murales = Mural::paginate(4);
+        $events = Event::where('fecha_evento', '>=', now())->with(['artists'])->paginate(4);
 
          return view('home', compact('home', 'artistas','events', 'murales'));
 
