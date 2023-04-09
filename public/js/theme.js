99455a39ecad00853b1acebbe0a5826b01ced4b9
Ayelén Iasich <87945150+AyelenIasich@users.noreply.github.com>
2023-04-08 21:11:33 -0300

    Changes theme

--- public/js/theme.js
+++ public/js/theme.js
@@ -2,22 +2,22 @@
 
 const themeBtn = document.querySelector(".theme-btn");
 themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
     themeBtn.classList.toggle("sun");
     localStorage.setItem("saved-theme", getCurrentTheme());
     localStorage.setItem("saved-icon", getCurrentIcon());
 });
 
 const getCurrentTheme = () =>
-    document.body.classList.contains("light-theme") ? "light" : "dark";
+    document.body.classList.contains("dark-theme") ? "dark" : "light";
 const getCurrentIcon = () =>
-    themeBtn.classList.contains("moon") ? "moon" : "sun";
+    themeBtn.classList.contains("sun") ? "sun" : "moon";
 const savedTheme = localStorage.getItem("saved-theme");
 const savedIcon = localStorage.getItem("saved-icon");
 
 if (savedTheme) {
-    document.body.classList[savedTheme === "light" ? "add" : "remove"](
-        "light-theme"
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+        "dark-theme"
     );
     themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
 }
