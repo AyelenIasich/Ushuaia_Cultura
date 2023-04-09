8e6f69ca3ea2b9f1368776e397e34e77520c195c
Ayelén Iasich <87945150+AyelenIasich@users.noreply.github.com>
2023-04-09 01:54:00 -0300

    Show the lastest created murales

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     {
        $home['home'] = Home::first();
         $artistas['artistas'] = Artista::all();
-        $murales = Mural::paginate(6);
+        $murales = Mural::latest('created_at')->paginate(6);
         $events = Event::where('fecha_evento', '>=', now())->with(['artists'])->paginate(4);
 
          return view('home', compact('home', 'artistas','events', 'murales'));
