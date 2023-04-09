bc84fd69c19cec0d88725c1898271ce7ab134f33
Ayelén Iasich <87945150+AyelenIasich@users.noreply.github.com>
2023-04-09 01:29:18 -0300

    Adding my own map

--- resources/views/components/mapa.blade.php
+++ resources/views/components/mapa.blade.php
@@ -4,27 +4,29 @@
             <h1 class="section-title-01 text-center display-4">Mapa</h1>
             <h2 class="section-title-02 text-center display-6">Mapa</h2>
         </div>
+        
         {{-- <div class="row mapa justify-content-center align-items-center">
 
         <iframe class="col-12 col-lg-9 text-center" src="https://www.google.com/maps/d/embed?mid=1bTPq6LgpRWdl7YNAtcP5Vt0sTKfUbUiV&ehbc=2E312F" ></iframe>
 
 </div> --}}
+
         <div class="row mapa justify-content-center align-items-center">
             <div class="col-12 col-lg-9 text-center ">
-                <a href="https://www.google.com/maps/d/embed?mid=1bTPq6LgpRWdl7YNAtcP5Vt0sTKfUbUiV&ehbc=2E312F">
+                <a href="https://www.google.com/maps/d/u/5/edit?mid=1sMd4B7bTofF2EEYlSvJQ3eLPpo3PmOM&usp=sharing">
                     <img class="col-12 col-lg-9 text-center" src="{{ asset('otros\EMUSH_MAPA.jpg') }}" alt="mapa de murales de emush">
                 </a>
 
 
             </div>
-            <div class="col-12 col-lg-9 text-center pt-3">
+          {{--   <div class="col-12 col-lg-9 text-center pt-3">
                 <small>Cortesía de @FaridTDF. Para más información visite su blog:<a
                         href="https://www.localguidesconnect.com/t5/General-Discussion/Murales-sorprendentes-en-Ushuaia/td-p/2932010">
                         Murales Sorprendentes en Ushuaia</a></small>
 
 
 
-            </div>
+            </div> --}}
         </div>
     </div>
 </section>
